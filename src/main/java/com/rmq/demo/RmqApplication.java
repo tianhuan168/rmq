b7f2f8f3bb1fc426c26cb44fe4286617a6102505
tianhuan <tianhuan@krspace.cn>
2019-04-28 17:31:50 +0800

    rmq

---
+++ src/main/java/com/rmq/demo/RmqApplication.java
@@ -0,0 +1,13 @@
+package com.rmq.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RmqApplication.class, args);
+    }
+
+}
